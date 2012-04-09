4308776515556c444f896abc92a695706bce8fa9
Peter Waller <peter.waller@cern.ch>
2012-04-09 16:33:40 +0100

    Make keyboard_reader.asm into a functioning example

--- keyboard_reader.asm
+++ keyboard_reader.asm
@@ -1,13 +1,21 @@
 ; Reading characters from the keyboard
 ; by Markus Persson
 
+#macro push(what) {
+    set push, what
+}
+
+#macro pop(what) {
+    set pop, what
+}
+
 #macro nextkey(target) {
 	push(i)
 	set i,[keypointer]
 	add i,0x9000
 	set target,[i]
 	ife target,0
-		jmp end
+		set pc, end
 	
 	set [i],0
 	add [keypointer], 1
@@ -15,6 +23,9 @@
 :end
 	pop(i)
 }
+
+nextkey(a)
+nextkey(b)
 
 :keypointer
 dat 0
